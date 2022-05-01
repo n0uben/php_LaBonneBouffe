46ef011ce5a87ebe14826fccb5fca0d232f2057b
n0uben <benjamin.noureux@gmail.com>
2022-05-01 13:45:05 +0200

    modifiecolonnes liste utilisateurs

--- src/views/liste-utilisateurs.php
+++ src/views/liste-utilisateurs.php
@@ -12,14 +12,16 @@
                 <table class="table table-striped">
                     <thead>
                     <th scope="col">#</th>
+                    <th scope="col">Email</th>
                     <th scope="col">Nom</th>
-                    <th scope="col">Unité</th>
+                    <th scope="col">Role</th>
                     </thead>
                     <tbody>
                     <?php
                     foreach ($utilisateurs as $utilisateur):
                         ?>
                         <tr>
+                            <td><?= $utilisateur->getId(); ?></td>
                             <td><?= $utilisateur->getEmail(); ?></td>
                             <td><?= $utilisateur->getNom(); ?></td>
                             <td><?= $utilisateur->getRole(); ?></td>
