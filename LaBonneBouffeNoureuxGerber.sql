4c862491c7241821019fd1ef88cfea55a43e9d91
n0uben <benjamin.noureux@gmail.com>
2022-05-09 11:42:50 +0200

    ajoute nouveau fichier sql

---
+++ LaBonneBouffeNoureuxGerber.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 09 mai 2022 à 11:41
+-- Version du serveur :  8.0.29-0ubuntu0.20.04.3
+-- Version de PHP : 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `LaBonneBouffeNoureuxGerber`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composition`
+--
+
+CREATE TABLE `composition` (
+  `id_ingredient` int NOT NULL,
+  `id_recette` int NOT NULL,
+  `quantite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `composition`
+--
+
+INSERT INTO `composition` (`id_ingredient`, `id_recette`, `quantite`) VALUES
+(1, 1, 500),
+(2, 1, 750);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Ingredient`
+--
+
+CREATE TABLE `Ingredient` (
+  `id` int NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `uniteMesure` enum('g','kg','cl','l','cac','cas','pincée') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `Ingredient`
+--
+
+INSERT INTO `Ingredient` (`id`, `nom`, `uniteMesure`) VALUES
+(1, 'Concombre', 'g'),
+(2, 'pommes de terre', 'g'),
+(14, 'tomates', 'g'),
+(15, 'cotes de porc', 'g');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Recette`
+--
+
+CREATE TABLE `Recette` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `categorie` varchar(255) NOT NULL,
+  `niveau` int NOT NULL,
+  `tpsPrepa` int NOT NULL,
+  `tpsCuisson` int NOT NULL,
+  `budget` varchar(50) NOT NULL,
+  `nbPers` int NOT NULL,
+  `etapes` text NOT NULL,
+  `utilisateurID` int NOT NULL,
+  `regionID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `Recette`
+--
+
+INSERT INTO `Recette` (`id`, `nom`, `categorie`, `niveau`, `tpsPrepa`, `tpsCuisson`, `budget`, `nbPers`, `etapes`, `utilisateurID`, `regionID`) VALUES
+(1, 'salade de concombre et pommes de terre', 'entrées', 1, 10, 20, 'pas cher', 2, '- cuire les pommes de terre\r\n- éplucher le concombre\r\n- couper le concombre en fines rondelles\r\n- idem pour les pdts\r\n- mélanger le tout dans un saladier avec un filet d’huile d’olive', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Region`
+--
+
+CREATE TABLE `Region` (
+  `id` int NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `Region`
+--
+
+INSERT INTO `Region` (`id`, `nom`) VALUES
+(1, 'Auvergne-Rhône-Alpes'),
+(2, 'Bourgogne-Franche-Comté'),
+(3, 'Bretagne'),
+(4, 'Centre-Val de Loire'),
+(5, 'Corse'),
+(6, 'Grand Est'),
+(7, 'Hauts-de-France'),
+(8, 'Île-de-France'),
+(9, 'Normandie'),
+(10, 'Nouvelle-Aquitaine'),
+(11, 'Occitanie'),
+(12, 'Pays de la Loire'),
+(13, 'Provence-Alpes-Côte d\'Azur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`id`, `email`, `mdp`, `nom`, `role`) VALUES
+(1, 'kevin@example.com', 'kevin', 'Kevin', 'admin'),
+(2, 'benjamin@example.com', 'benjamin', 'Benjamin', 'admin');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `composition`
+--
+ALTER TABLE `composition`
+  ADD PRIMARY KEY (`id_ingredient`,`id_recette`),
+  ADD KEY `id_recette` (`id_recette`);
+
+--
+-- Index pour la table `Ingredient`
+--
+ALTER TABLE `Ingredient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Recette`
+--
+ALTER TABLE `Recette`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `utilisateurID` (`utilisateurID`),
+  ADD KEY `regionID` (`regionID`);
+
+--
+-- Index pour la table `Region`
+--
+ALTER TABLE `Region`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Ingredient`
+--
+ALTER TABLE `Ingredient`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT pour la table `Recette`
+--
+ALTER TABLE `Recette`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `Region`
+--
+ALTER TABLE `Region`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `composition`
+--
+ALTER TABLE `composition`
+  ADD CONSTRAINT `composition_ibfk_1` FOREIGN KEY (`id_ingredient`) REFERENCES `Ingredient` (`id`),
+  ADD CONSTRAINT `composition_ibfk_2` FOREIGN KEY (`id_recette`) REFERENCES `Recette` (`id`);
+
+--
+-- Contraintes pour la table `Recette`
+--
+ALTER TABLE `Recette`
+  ADD CONSTRAINT `id` FOREIGN KEY (`regionID`) REFERENCES `Region` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
