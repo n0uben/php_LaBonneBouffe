8e2bbebc7b7a9ee4049767e80b83e3f38e7c0f7d
n0uben <benjamin.noureux@gmail.com>
2022-05-09 12:48:02 +0200

    ajoute icones fontawesome

--- src/view/general/header.php
+++ src/view/general/header.php
@@ -11,8 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../../../public/css/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/461b8e3720.js" crossorigin="anonymous"></script>
 
-    <title>Document</title>
+    <title>La bonne bouffe</title>
 </head>
 <body>
     <header>
@@ -20,7 +21,7 @@
             <div class="row">
                 <div class="col">
                     <h1>La bonne bouffe</h1>
-                    <p">Le super site des recettes</p>
+                    <p">Le super site des recettes du terroir</p>
                 </div>
             </div>
         </div>
