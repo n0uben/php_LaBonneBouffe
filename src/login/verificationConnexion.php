956b7876ebcc622ffc73851232221de0c3fb0a12
KevinG57 <gerber.kevin@live.fr>
2022-05-14 18:33:12 +0200

    erreur 500 avec verifCo

--- src/login/verificationConnexion.php
+++ src/login/verificationConnexion.php
@@ -1,45 +1,46 @@
 <?php
+require_once __DIR__.'./../model/manager/DbManager.php';
+//require_once  __DIR__.'./../model/manager/UtilisateurManager.php';
 session_start();
 if(isset($_POST['username']) && isset($_POST['password']))
 {
-    // connexion à la base de données
-    $db_username = 'root';
-    $db_password = '';
-    $db_name     = 'LaBonneBouffe';
-    $db_host     = 'localhost';
-    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-    or die('could not connect to database');
+
+    $db = DbManager::DBConnect();
 
     // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
-    // pour éliminer toute attaque de type injection SQL et XSS
-    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username']));
-    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    // pour eviter les attaques de type injections SQL ou XSS
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
 
     if($username !== "" && $password !== "")
     {
-        $requete = "SELECT count(*) FROM Utilisateur where 
-              email = '".$username."' and mdp = '".$password."' ";
-        $exec_requete = mysqli_query($db,$requete);
-        $reponse      = mysqli_fetch_array($exec_requete);
-        $count = $reponse['count(*)'];
-        if($count!=0) // nom d'utilisateur et mot de passe correctes
+//        $utilisateurManager = new UtilisateurManager();
+//        $utilisateur = $utilisateurManager->getByEmail($username);
+
+
+//        $requete = "SELECT count(*) FROM Utilisateur where
+//              email = '".$username."' and mdp = '".$password."' ";
+//        $exec_requete = mysqli_query($db,$requete);
+//        $reponse      = mysqli_fetch_array($exec_requete);
+//        $count = $reponse['count(*)'];
+
+
+        if(1)//$utilisateur->getMdp() == $password) // mot de passe correctes
         {
             $_SESSION['username'] = $username;
-            header('Location: index.php');
+            header('Location: http://localhost/php_LaBonneBouffe/index.php');
         }
         else
         {
-            header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+            header('Location: http://localhost/php_LaBonneBouffe/login.php?erreur=1'); // utilisateur ou mot de passe incorrect
         }
     }
     else
     {
-        header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+        header('Location: http://localhost/php_LaBonneBouffe/login.php?erreur=2'); // utilisateur ou mot de passe vide
     }
 }
 else
 {
-    header('Location: login/login.php');
+    header('Location: http://localhost/php_LaBonneBouffe/login.php');
 }
-mysqli_close($db); // fermer la connexion
-?>
