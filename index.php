e960b9c794edda9e9ce64ae5bcbbb39c3a7e541f
n0uben <benjamin.noureux@gmail.com>
2022-05-18 23:54:43 +0200

    remet auth

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 session_start();
 // Test d'une session ouverte : si pas de session ouverte avec email, retour sur login.php
-//if (!isset($_SESSION['email'])) {
-//    header('Location: ./login.php');
-//}
+if (!isset($_SESSION['email'])) {
+    header('Location: ./login.php');
+}
 
 require './config.php';
 require_once './src/view/general/header.php';
